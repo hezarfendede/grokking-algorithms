fb3a4ec912156b50fb17b1d0dc2ff6e8facfcf8f
Bahadır Sivrikaya <bahadir.sivrikaya@transavia.com>
2021-11-05 13:31:12 +0100

    added selection sort

---
+++ src/Algorithms/SelectionSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Algorithms.Sort
+{
+    public class SelectionSort
+    {
+        public static int[] Sort(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException($"The {nameof(array)} is null");
+            }
+
+            int smallest = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+
+                Swap(array, i, smallest);
+            }
+
+            return array;
+        }
+
+        private static void Swap(int[] array, int x, int y)
+        {
+            int temp = array[x];
+            array[x] = array[y];
+            array[y] = temp;
+        }
+    }
+}
---
+++ tst/Tests.Algorithms/SelectionSortTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using Algorithms.Sort;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.Algorithms.Sort
+{
+    public class SelectionSortTests
+    {
+        private readonly int[] _unsortedArray =
+        {
+            27, 17, 25, 4, 1, 3, 2, 6, 11, 12, 9, 7, 15, 5, 13, 17, 20, 16, 18, 14, 22, 23, 21
+        };
+
+        [Fact(DisplayName = "SST-001: SelectionSort throws an ArgumentNullException when the array is null.")]
+        public void SST001()
+        {
+            // assign
+
+            // act
+            Action act = () => SelectionSort.Sort(null);
+
+            // assert
+            act.Should()
+                .Throw<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "SST-002: SelectionSort sorts an unsorted array correctly.")]
+        public void SST002()
+        {
+            // assign
+            var temp = _unsortedArray.ToList();
+            temp.Sort();
+            var expectedSortedArray = temp.ToArray();
+
+            // act
+            var result = SelectionSort.Sort(_unsortedArray);
+
+            // assert
+            result.Should().BeEquivalentTo(expectedSortedArray);
+        }
+
+        //[Fact(DisplayName = "BST-003: BinarySearch can find the index of an item that exists in the array.")]
+        //public void BST003()
+        //{
+        //    // assign
+        //    int item = 7;
+
+        //    // act
+        //    var result = BinarySearch.Search(_sortedArray, item);
+
+        //    // assert
+        //    result.Should().Be(6);
+        //}
+    }
+}
