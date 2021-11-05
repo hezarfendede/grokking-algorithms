ffd4383f3d3507aa3c22e2281cd5a5054f868849
Bahadır Sivrikaya <bahadir.sivrikaya@transavia.com>
2021-11-05 15:03:46 +0100

    added Factorial

---
+++ src/Algorithms/Factorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algorithms.Calculations
+{
+    public class Factorial
+    {
+        public static int Calculate(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(number)} can't be negative.'");
+            }
+
+            if (number > 10)
+            {
+                throw new NotImplementedException($"{nameof(number)} can't be larger than 10.'");
+            }
+
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Calculate(number - 1);
+        }
+    }
+}
---
+++ tst/Tests.Algorithms/FactorialTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Algorithms.Calculations;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.Algorithms.Calculations
+{
+    public class FactorialTests
+    {
+        [Fact(DisplayName = "FT-001: Factorial opeation throws an ArgumentOutOfRangeException when the number is negative.")]
+        public void FT001()
+        {
+            // assign
+
+            // act
+            Action act = () => Factorial.Calculate(-1);
+
+            // assert
+            act.Should()
+                .Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Theory(DisplayName = "FT-002: Factorial operation return the correct result")]
+        [InlineData(0, 1)]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 6)]
+        [InlineData(4, 24)]
+        [InlineData(5, 120)]
+        [InlineData(6, 720)]
+        [InlineData(7, 5040)]
+        [InlineData(8, 40320)]
+        [InlineData(9, 362880)]
+        [InlineData(10, 3628800)]
+        public void FT002(int input, int expected)
+        {
+            // assign
+
+            // act
+            var result = Factorial.Calculate(input);
+
+            // assert
+            result.Should().Be(expected);
+        }
+
+        [Fact(DisplayName = "FT-003: Factorial opeation throws NotImplementedException when the number is larger than 10.")]
+        public void FT003()
+        {
+            // assign
+
+            // act
+            Action act = () => Factorial.Calculate(11);
+
+            // assert
+            act.Should()
+                .Throw<NotImplementedException>();
+        }
+    }
+}
