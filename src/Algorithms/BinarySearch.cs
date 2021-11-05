f0e5ef0a8e8bfc1b023c8142c951f79f349c62f3
Bahadır Sivrikaya <bahadir.sivrikaya@transavia.com>
2021-11-05 12:05:55 +0100

    added binary search

---
+++ src/Algorithms/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class BinarySearch
+    {
+        public static int? Search(int[] list, int item)
+        {
+            if (list == null)
+            {
+                throw new ArgumentNullException("The list is null");
+            }
+
+            int low = 0;
+            int high = list.Length;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                int guess = list[mid];
+
+                if (guess == item)
+                {
+                    return mid;
+                }
+
+                if (guess > item)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ tst/Tests.Algorithms/BinarySearchTests.cs
@@ -0,0 +1,54 @@
+using System;
+using Algorithms;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.Algorithms
+{
+    public class BinarySearchTests
+    {
+        private readonly int[] _sortedArray =
+        {
+            1, 2, 3, 4, 5, 6, 7, 9, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 25, 26, 27
+        };
+
+        [Fact(DisplayName = "BST-001: BinarySearch throws an ArgumentNullException when the array is null.")]
+        public void BST001()
+        {
+            // assign
+
+            // act
+            Action act = () => BinarySearch.Search(null, 8);
+
+            // assert
+            act.Should()
+                .Throw<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "BST-002: BinarySearch can't found an item that does not exist in the array.")]
+        public void BST002()
+        {
+            // assign
+            int item = 8;
+
+            // act
+            var result = BinarySearch.Search(_sortedArray, item);
+
+            // assert
+            result.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "BST-003: BinarySearch can found the index of an item that exists in the array.")]
+        public void BST003()
+        {
+            // assign
+            int item = 7;
+
+            // act
+            var result = BinarySearch.Search(_sortedArray, item);
+
+            // assert
+            result.Should().Be(6);
+        }
+    }
+}
